f92579426446ab000267fbec3a3b6d2c02602df9
MayceAlazzawi <maynab4@gmail.com>
2022-06-06 14:08:10 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+
+class Polygon {
+  constructor(array) {
+    this.array = array;
+  }
+  get countSides() {
+    return this.array.length - 1;
+  }
+  get permiter() {
+    let sum = 0;
+    for (let i = 0; i < this.array.length; i++) {
+      sum += this.array[i]
+      console.log(sum)
+    }
+  }
+}
+const arr = new Polygon([1, 2, 3])
+console.log(arr.permiter)
+
+class triangle extends from {
+  // check condition
+
+}
+
+
