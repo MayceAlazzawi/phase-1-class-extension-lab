a7af53f66b08be8a9fadd88bc2caca23b81f8a8f
MayceAlazzawi <maynab4@gmail.com>
2022-06-07 13:39:30 +0300

    done

--- index.js
+++ index.js
@@ -1,25 +1,59 @@
 
+// class Polygon {
+//   constructor(array) {
+//     this.array = array;
+//   }
+//   get countSides() {
+//     return this.array.length - 1;
+//   }
+//   get permiter() {
+//     let sum = 0;
+//     for (let i = 0; i < this.array.length; i++) {
+//       sum += this.array[i]
+//       console.log(sum)
+//     }
+//   }
+// }
+// const arr = new Polygon([1, 2, 3])
+// console.log(arr.permiter)
+
+// class triangle extends from {
+//   // check condition
+
+// }
+
+
+// Your code here
 class Polygon {
-  constructor(array) {
-    this.array = array;
+  constructor(shapeArr) {
+    this.shapeArr = shapeArr
   }
   get countSides() {
-    return this.array.length - 1;
+    return this.shapeArr.length
   }
-  get permiter() {
-    let sum = 0;
-    for (let i = 0; i < this.array.length; i++) {
-      sum += this.array[i]
-      console.log(sum)
-    }
+  get perimeter() {
+    return this.shapeArr.reduce(function (prev, curr) {
+      return prev + curr
+    })
   }
 }
-const arr = new Polygon([1, 2, 3])
-console.log(arr.permiter)
 
-class triangle extends from {
-  // check condition
-
+class Triangle extends Polygon {
+  // constructor(shapeArr){
+  //   super(shapeArr)
+  // }
+  get isValid() {
+    let sides = this.shapeArr
+    return (sides[0] + sides[1] > sides[2]) && (sides[0] + sides[2] > sides[1]) && (sides[1] + sides[2] > sides[0])
+  }
 }
 
-
+class Square extends Polygon {
+  get isValid() {
+    let sides = this.shapeArr
+    return (sides[0] ** 4) === (sides.reduce((prev, curr) => prev * curr))
+  }
+  get area() {
+    return this.shapeArr[0] ** 2
+  }
+}
